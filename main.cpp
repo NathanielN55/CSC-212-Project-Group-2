7912fed0bbf29734f2767a2bfbec05777a2d0051
NathanielN55 <123680682+NathanielN55@users.noreply.github.com>
2023-07-24 18:30:15 -0400

    Added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@
 void print_tree(LLRBTree* tree, int mode, std::ofstream& os);
 
 int main(int argc, char *argv[]){
+    // Input file name
     ifstream infile(argv[1]);
+    // Mode number
     int mode = stoi(argv[2]);
+    // Names of the output files
     string txtname(argv[3]);
     string dotname(argv[3]);
     txtname += ".txt";
     dotname += ".dot";
 
+    // Open the text file output stream
     ofstream txtfile(txtname);
 
+    // Create a new tree
     LLRBTree tree;
 
     string line;
@@ -22,21 +27,24 @@ int main(int argc, char *argv[]){
     getline(infile, line);
     stringstream ss(line);
 
+    // Insert each number in the input file into the tree
     int num;
     while (ss >> num) {
         tree.insert(num);
 
     }
 
+    // Print the necessary tree info to the text file
     print_tree(&tree, mode, txtfile);
     txtfile << "Tree Height: " << tree.height() << endl;
+    // Close the text file output stream
     txtfile.close();
 
-
+    // Open the dot file output stream
     ofstream dotfile(dotname);
-
+    // Generate the dot file 
     tree.generateDotFile(dotfile);
-
+    // Close the dot file
     dotfile.close();
     
 }
