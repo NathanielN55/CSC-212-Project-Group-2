3e2ee5ebdb5e0d489f79e2e92713d4a50efaa0bf
jackep1 <98371960+jackep1@users.noreply.github.com>
2023-07-24 15:35:25 -0400

    Comments to .h file

--- llrbtree.h
+++ llrbtree.h
@@ -15,12 +15,17 @@ using namespace std;
 #define MAX(a,b) ((a > b) ? a : b)
 #endif
 
+// Class of nodes for the tree
 class LLRBNode {
 private:
+    // Holds the relevant data
     int data;
+    // Determines if this node is red (true) or black (false)
     bool red;
+    // Pointers to this node's left and right child nodes
     LLRBNode* left;
     LLRBNode* right;
+    // Private setter for changing color of the node
     void flip_color();
 public:
     LLRBNode();
@@ -32,15 +37,23 @@ public:
 
 class LLRBTree {
 private:
+    // Tree's root node, from which all others are accessed via pointers
     LLRBNode* root;
+
+    // Collection of methods for manipulating data, defined privately
+    // so that they can be called within public versions of these functions
+    // without allowing manual manipulation of the tree
     LLRBNode* insert(int data, LLRBNode* root);
     LLRBNode* remove(int data, LLRBNode* root);
     LLRBNode* find_ios(LLRBNode* root, bool &disconnect);
+
+    // Important functions for rotating the tree when necessary
     LLRBNode* rotate_left(LLRBNode* root);
     LLRBNode* rotate_right(LLRBNode* root);
     int height(LLRBNode* root);
-
     void flip_color(LLRBNode* root);
+
+    // Functions for printing the tree in various named orders
     void preorder(LLRBNode* root, ostream &os);
     void inorder(LLRBNode* root, ostream &os);
     void postorder(LLRBNode* root, ostream &os);
@@ -52,7 +65,8 @@ private:
 public:
     LLRBTree();
     ~LLRBTree();
-
+    // Functions for manipulating the tree which call the
+    // private functions in order to maintain the tree's integrity
     void insert(int data);
     void remove(int data);
     int height();
